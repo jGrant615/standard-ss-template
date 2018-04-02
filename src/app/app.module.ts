b1a200c6163731ba7c94b5310acc33098caf7bde
Justus Grant <grantju4@msu.edu>
2018-04-01 21:07:19 -0400

    initiating relevant components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Packages
+import { gateway as MoltinGateway } from '@moltin/sdk';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { UserComponent } from './user/user.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ProductComponent,
+    OrderComponent,
+    CheckoutComponent,
+    UserComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
@@ -20,3 +33,7 @@ import { FooterComponent } from './footer/footer.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const Moltin = MoltinGateway({
+  client_id: '91EoW1OO88Q4ecPPyaRy6qMffDC3Il0NwN5xuuZ8gP'
+})
